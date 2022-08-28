8be9905eb798a183242a0689fd83b49398543f39
JoaoGripp <corguinha@gmail.com>
2022-08-28 07:37:26 -0300

    Initial Commit

---
+++ iOSTakeHomeProject/iOSTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTakeHomeProjectApp.swift
+//  iOSTakeHomeProject
+//
+//  Created by Joao Gripp on 28/08/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
