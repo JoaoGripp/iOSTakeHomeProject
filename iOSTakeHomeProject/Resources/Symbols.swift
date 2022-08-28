73ffed971b12a51c6cd3a18051a9763e6b9c7f2a
JoaoGripp <corguinha@gmail.com>
2022-08-28 07:58:20 -0300

    Create the project and add some resources enum for Color and SFSymbols - Finish Lecture #3

---
+++ iOSTakeHomeProject/Resources/Symbols.swift
@@ -0,0 +1,16 @@
+//
+//  Symbols.swift
+//  iOSTakeHomeProject
+//
+//  Created by Joao Gripp on 28/08/22.
+//
+
+import SwiftUI
+
+enum Symbols {
+    static let person   = Image(systemName: "person.2")
+    static let gear     = Image(systemName: "gear")
+    static let plus     = Image(systemName: "plus")
+}
+
+
---
+++ iOSTakeHomeProject/Resources/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  iOSTakeHomeProject
+//
+//  Created by Joao Gripp on 28/08/22.
+//
+
+import SwiftUI
+
+enum Theme {
+    static let background               = Color("background")
+    static let detailBackground         = Color("detail-background")
+    static let text                     = Color("text")
+    static let pill                     = Color("pill")
+}
