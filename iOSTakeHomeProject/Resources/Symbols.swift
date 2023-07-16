8a549edea1a74250e15e8aae6ab26f3e2cc23343
Joao Gripp <corguinha@gmail.com>
2023-07-16 14:26:29 +0100

    Finished #28 - Adding an animated Pop-Up

--- iOSTakeHomeProject/Resources/Symbols.swift
+++ iOSTakeHomeProject/Resources/Symbols.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 enum Symbols {
-    static let person   = Image(systemName: "person.2")
-    static let gear     = Image(systemName: "gear")
-    static let plus     = Image(systemName: "plus")
-    static let link     = Image(systemName: "link")
+    static let person       = Image(systemName: "person.2")
+    static let gear         = Image(systemName: "gear")
+    static let plus         = Image(systemName: "plus")
+    static let link         = Image(systemName: "link")
+    static let checkmark    = Image(systemName: "checkmark")
 }
 
 
