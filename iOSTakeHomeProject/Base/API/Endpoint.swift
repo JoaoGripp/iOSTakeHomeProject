a616febbb5fb19468d5c64263e29aeccbb21e0c6
Joao Gripp <corguinha@gmail.com>
2023-08-03 00:07:25 +0100

    Finished #41 - Unite Testing, Concurrency

--- iOSTakeHomeProject/Base/API/Endpoint.swift
+++ iOSTakeHomeProject/Base/API/Endpoint.swift
@@ -14,7 +14,7 @@ enum Endpoint {
 }
 
 extension Endpoint {
-    enum MethodType {
+    enum MethodType: Equatable {
         case GET
         case POST(data: Data?)
     }
@@ -61,13 +61,15 @@ extension Endpoint {
         urlComponents.host = host
         urlComponents.path = path
         
-        var requestQueryItems = queryItems?.compactMap { item in
-            URLQueryItem(name: item.key, value: item.value)
+        var requestQueryItems = [URLQueryItem]()
+        
+        queryItems?.forEach{ item in
+            requestQueryItems.append(URLQueryItem(name: item.key, value: item.value))
         }
         
         //run only on Debug mode
         #if DEBUG
-        requestQueryItems?.append(URLQueryItem(name: "delay", value: "8"))
+        requestQueryItems.append(URLQueryItem(name: "delay", value: "8"))
         #endif
         
         urlComponents.queryItems = requestQueryItems
